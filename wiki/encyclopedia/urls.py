9f14d0c8cdceb62dd9b83bc78adc409b8deb76d1
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-07-04 23:38:45 +0330

    add edit/create entry and choice random entry

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("new_wiki/", views.create_entry, name="create_entry"),
+    path("edit_entry/<str:title>/", views.edit_entry, name="edit_entry"),
+    path("random_entry/", views.random_entry, name="random_entry"),
     path("wiki/<str:title>/", views.entry, name="entry"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,10 @@
+import random
+
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django import forms
+from django.contrib import messages
 
 from . import util
 
@@ -30,3 +34,54 @@ def entry(request, title):
     return render(request, "encyclopedia/entry.html", {
         "title": title, "entry": entry
     })
+
+
+class EntryForm(forms.Form):
+    title = forms.CharField(max_length=255, label="Title")
+    text = forms.CharField(label="content", widget=forms.Textarea(attrs=
+                {'placeholder': 'Type your text as Markdown'}))
+
+def create_entry(request):
+    if request.method == "POST":
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            text = form.cleaned_data["text"]
+            if util.get_entry(title):
+                message_text = "The entry with this title already exist! " \
+                                "please choose another title"
+                messages.error(request, message_text)
+                return render(request, "encyclopedia/entry_form.html", {
+                    "form":form
+                })
+
+            else:
+                util.save_entry(title, text)
+                return HttpResponseRedirect(reverse("entry", args=[title]))
+    
+    return render(request, "encyclopedia/entry_form.html", {
+        "form":EntryForm()
+    })
+
+
+def edit_entry(request, title):
+    content = util.get_entry(title)
+    form = EntryForm(initial={"title":title, "text": content})
+
+    if request.method == "POST":
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            text = form.cleaned_data["text"]
+            util.save_entry(title, text)
+            return HttpResponseRedirect(reverse("entry", args=[title]))
+    
+    return render(request, "encyclopedia/entry_form.html", {
+        "form":form
+    })
+
+
+def random_entry(request):
+    entries = util.list_entries()
+    title = random.choice(entries)
+    return HttpResponseRedirect(reverse("entry", args=[title]))
