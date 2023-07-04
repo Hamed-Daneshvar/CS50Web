6d6ef36c13873c5ca7ef787d6117badbae79ce6c
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-07-04 21:36:24 +0330

    entry and index page and search feature was added

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 from . import util
 
+from markdown2 import Markdown
+
 
 def index(request):
+    entries = util.list_entries()
+    q = request.GET.get('q', '')
+
+    if q:
+        if q in entries:
+            return HttpResponseRedirect(reverse("entry", args=[q]))
+        else:
+            entries = [title for title in entries if q in title]
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries
     })
 
+
+def entry(request, title):
+    markdowner = Markdown()
+    entry = util.get_entry(title)
+    if entry:
+        entry = markdowner.convert(util.get_entry(title))
+    return render(request, "encyclopedia/entry.html", {
+        "title": title, "entry": entry
+    })
